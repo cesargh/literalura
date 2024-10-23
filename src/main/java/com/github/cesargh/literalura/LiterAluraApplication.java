c73cbad49dbd115a583c93754c8aab7bb8ccacbe
cesargh <cesar.joblink@gmail.com>
2024-10-23 20:17:04 -0300

    Configuración del proyecto.

--- src/main/java/com/github/cesargh/literalura/LiterAluraApplication.java
+++ src/main/java/com/github/cesargh/literalura/LiterAluraApplication.java
@@ -1,13 +1,19 @@
 package com.github.cesargh.literalura;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LiterAluraApplication {
+public class LiterAluraApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiterAluraApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
 	}
 
 }
