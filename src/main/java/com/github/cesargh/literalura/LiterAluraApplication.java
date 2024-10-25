d4b1755ec8c73ec5cc48bf661f332510d7c2f0c9
cesargh <cesar.joblink@gmail.com>
2024-10-25 15:05:27 -0300

    Utilización de LibroController en crudo para realizar pruebas.

--- src/main/java/com/github/cesargh/literalura/LiterAluraApplication.java
+++ src/main/java/com/github/cesargh/literalura/LiterAluraApplication.java
@@ -1,23 +1,50 @@
 package com.github.cesargh.literalura;
 
-import com.github.cesargh.literalura.service.Buscador;
+import com.github.cesargh.literalura.controller.LibroController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootApplication
 public class LiterAluraApplication implements CommandLineRunner {
+
+	@Autowired
+	LibroController libroController;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiterAluraApplication.class, args);
 	}
 
+	@Transactional(propagation = Propagation.NEVER)
 	@Override
 	public void run(String... args) throws Exception {
 
-		System.out.println("DEBUG : Inicio de Buscador.BuscarLibrosPorTitulo");
-		System.out.println(Buscador.BuscarLibrosPorTitulo("San Francisco"));
-		System.out.println("DEBUG : Fin de Buscador.BuscarLibrosPorTitulo");
+		String titulo = "DEBUG";
+		//titulo = "San Francisco";
+		//titulo = "Fran";
+		//titulo = "San FRANCISCO";
+		//titulo = "FRANCIS";
+		//titulo = "franç";
+		//titulo = "CAÑO";
+		//titulo = "año";
+		//titulo = "traducción";
+		//titulo = "ción";
+		//titulo = "cion";
+		//titulo = "Frankenstein; Or, The Modern Prometheus";
+
+		System.out.println("DEBUG : Buscador.BuscarLibrosPorTitulo --> INICIO");
+		var biblioteca = libroController.BuscarLibrosPorTitulo(titulo);
+		if (biblioteca.isEmpty()) {
+			System.out.println("DEBUG : Buscador.BuscarLibrosPorTitulo --> EMPTY");
+		} else {
+			for(Object libro : biblioteca.get()) {
+				System.out.printf("DEBUG : Buscador.BuscarLibrosPorTitulo --> %s\n", libro);
+			}
+		}
+		System.out.println("DEBUG : Buscador.BuscarLibrosPorTitulo --> FIN");
 
 	}
 
